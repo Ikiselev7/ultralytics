fe54f1de6fafd924fd41ee2de1feacc435637ad4
Ilia Kiselev <ikiselev7@gmail.com>
2023-07-15 01:31:36 +0200

    apply dice loss

--- ultralytics/yolo/v8/segment/train.py
+++ ultralytics/yolo/v8/segment/train.py
@@ -131,7 +131,8 @@ class SegLoss(Loss):
                     xyxyn = target_bboxes[i][fg_mask[i]] / imgsz[[1, 0, 1, 0]]
                     marea = xyxy2xywh(xyxyn)[:, 2:].prod(1)
                     mxyxy = xyxyn * torch.tensor([mask_w, mask_h, mask_w, mask_h], device=self.device)
-                    loss[1] += self.single_mask_dice_loss(gt_mask, pred_masks[i][fg_mask[i]], proto[i], mxyxy, marea)  # seg
+                    loss[1] += 0.5 * self.single_mask_dice_loss(gt_mask, pred_masks[i][fg_mask[i]], proto[i], mxyxy, marea)  # seg
+                    loss[1] += 0.5 * self.single_mask_loss(gt_mask, pred_masks[i][fg_mask[i]], proto[i], mxyxy, marea)  # seg
 
                 # WARNING: lines below prevents Multi-GPU DDP 'unused gradient' PyTorch errors, do not remove
                 else:
