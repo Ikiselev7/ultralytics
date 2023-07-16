f445d454bfa7f32d98e9ec6874343a4f2ea5042c
Ilia Kiselev <ikiselev7@gmail.com>
2023-07-16 13:22:24 +0200

    full mask dice + crop bce

--- ultralytics/yolo/v8/segment/train.py
+++ ultralytics/yolo/v8/segment/train.py
@@ -131,8 +131,9 @@ class SegLoss(Loss):
                     xyxyn = target_bboxes[i][fg_mask[i]] / imgsz[[1, 0, 1, 0]]
                     marea = xyxy2xywh(xyxyn)[:, 2:].prod(1)
                     mxyxy = xyxyn * torch.tensor([mask_w, mask_h, mask_w, mask_h], device=self.device)
-                    loss[1] += 0.5 * self.single_mask_dice_loss(gt_mask, pred_masks[i][fg_mask[i]], proto[i], mxyxy, marea)  # seg
-                    loss[1] += 0.5 * self.single_mask_loss(gt_mask, pred_masks[i][fg_mask[i]], proto[i], mxyxy, marea)  # seg
+                    pred_mask = (pred_masks[i][fg_mask[i]] @ proto[i].view(self.nm, -1)).view(-1, *proto.shape[1:])
+                    loss[1] += 0.5 * self.single_mask_dice_loss(gt_mask, pred_mask, mxyxy, marea)  # seg
+                    loss[1] += 0.5 * self.single_mask_loss(gt_mask, pred_mask, mxyxy, marea)  # seg
 
                 # WARNING: lines below prevents Multi-GPU DDP 'unused gradient' PyTorch errors, do not remove
                 else:
@@ -149,16 +150,14 @@ class SegLoss(Loss):
 
         return loss.sum() * batch_size, loss.detach()  # loss(box, cls, dfl)
 
-    def single_mask_loss(self, gt_mask, pred, proto, xyxy, area):
+    def single_mask_loss(self, gt_mask, pred_mask, xyxy, area):
         """Mask loss for one image."""
-        pred_mask = (pred @ proto.view(self.nm, -1)).view(-1, *proto.shape[1:])  # (n, 32) @ (32,80,80) -> (n,80,80)
         loss = F.binary_cross_entropy_with_logits(pred_mask, gt_mask, reduction='none')
         return (crop_mask(loss, xyxy).mean(dim=(1, 2)) / area).mean()
         # return (loss.mean(dim=(1, 2)) / area).mean()
 
-    def single_mask_dice_loss(self, gt_mask, pred, proto, xyxy, area):
+    def single_mask_dice_loss(self, gt_mask, pred_mask, xyxy, area):
         """Mask loss for one image."""
-        pred_mask = (pred @ proto.view(self.nm, -1)).view(-1, *proto.shape[1:])
         pred_mask = pred_mask.sigmoid()
         # pred_mask = crop_mask(pred_mask, xyxy)
         # gt_mask = crop_mask(gt_mask, xyxy)
