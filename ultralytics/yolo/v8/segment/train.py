9f6f11242b14a91c476c922a56a9726e884e8f46
Ilia Kiselev <ikiselev7@gmail.com>
2023-07-16 14:09:39 +0200

    full mask dice + crop bce

--- ultralytics/yolo/v8/segment/train.py
+++ ultralytics/yolo/v8/segment/train.py
@@ -131,7 +131,7 @@ class SegLoss(Loss):
                     xyxyn = target_bboxes[i][fg_mask[i]] / imgsz[[1, 0, 1, 0]]
                     marea = xyxy2xywh(xyxyn)[:, 2:].prod(1)
                     mxyxy = xyxyn * torch.tensor([mask_w, mask_h, mask_w, mask_h], device=self.device)
-                    pred_mask = (pred_masks[i][fg_mask[i]] @ proto[i].view(self.nm, -1)).view(-1, *proto.shape[1:])
+                    pred_mask = (pred_masks[i][fg_mask[i]] @ proto[i].view(self.nm, -1)).view(-1, *proto[i].shape[1:])
                     loss[1] += 0.5 * self.single_mask_dice_loss(gt_mask, pred_mask, mxyxy, marea)  # seg
                     loss[1] += 0.5 * self.single_mask_loss(gt_mask, pred_mask, mxyxy, marea)  # seg
 
