77060f018718909aefcaf22585c606595ef04469
Ilia Kiselev <ikiselev7@gmail.com>
2023-07-18 19:31:12 +0200

    Allpy SoftTal

--- ultralytics/yolo/utils/tal.py
+++ ultralytics/yolo/utils/tal.py
@@ -372,29 +372,21 @@ class SoftTaskAlignedAssigner(nn.Module):
         na = pd_bboxes.shape[-2]
         mask_gt = mask_gt.bool()  # b, max_num_obj, h*w
         overlaps = torch.zeros([self.bs, self.n_max_boxes, na], dtype=pd_bboxes.dtype, device=pd_bboxes.device)
-        bbox_scores = torch.zeros([self.bs, self.n_max_boxes, na, pd_scores.size(2)], dtype=pd_scores.dtype, device=pd_scores.device)
+        bbox_scores = torch.zeros([self.bs, self.n_max_boxes, na], dtype=pd_scores.dtype, device=pd_scores.device)
 
         ind = torch.zeros([2, self.bs, self.n_max_boxes], dtype=torch.long)  # 2, b, max_num_obj
         ind[0] = torch.arange(end=self.bs).view(-1, 1).expand(-1, self.n_max_boxes)  # b, max_num_obj
         ind[1] = gt_labels.squeeze(-1)  # b, max_num_obj
         # Get the scores of each grid for each gt cls
-        bbox_scores[mask_gt] = pd_scores[ind[0], :][mask_gt]  # b, max_num_obj, h*w
-
-        bbox_scores[mask_gt] = (bbox_scores * \
-                      self._smooth(torch.nn.functional.one_hot(
-                          gt_labels.expand([self.bs, self.n_max_boxes, na]).long(),
-                          pd_scores.size(2)
-                      ).to(bbox_scores.dtype), mask_gt, 0.1))[mask_gt]
+        bbox_scores[mask_gt] = pd_scores[ind[0], :, ind[1]][mask_gt]  # b, max_num_obj, h*w
 
         # (b, max_num_obj, 1, 4), (b, 1, h*w, 4)
         pd_boxes = pd_bboxes.unsqueeze(1).expand(-1, self.n_max_boxes, -1, -1)[mask_gt]
         gt_boxes = gt_bboxes.unsqueeze(2).expand(-1, -1, na, -1)[mask_gt]
         overlaps[mask_gt] = bbox_iou(gt_boxes, pd_boxes, xywh=False, CIoU=True).squeeze(-1).clamp_(0)
 
-        overlaps = overlaps.unsqueeze(-1)
-
         align_metric = bbox_scores.pow(self.alpha) * overlaps.pow(self.beta)
-        return align_metric.amax(dim=3), overlaps.squeeze(-1)
+        return align_metric, overlaps
 
     def select_topk_candidates(self, metrics, largest=True, topk_mask=None):
         """
